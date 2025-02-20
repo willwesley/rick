2130a0d8302ca88fc038a1c29f07b6bedefeb90a
Will Wesley <will@knze.net>
2025-02-19 17:58:03 -0700

    Class work 2025-02-19

--- jsapp/index.js
+++ jsapp/index.js
@@ -2,8 +2,31 @@
 
 const http = require('http')
 
+const dancers = []
+
 const handleRequest = (req, res) => {
-  res.write("Golly.")
+  const [path, query] = req.url.split('?')
+  if(req.method == "POST") {
+    let body = ''
+    req.on('data', (data) => {
+      body += data
+    })
+    req.on('end', () => {
+      const params = Object.fromEntries(body.split('&').map(
+        (param) => param.split('=')
+      ))
+      dancers.push(params)
+      kbye(res)
+    })
+  } else {
+    kbye(res)
+  }
+}
+const kbye = (res) => {
+  res.writeHead(200, {
+    "Content-Type": "application/json"
+  })
+  res.write(JSON.stringify(dancers))
   res.end()
 }
 const server = http.createServer(handleRequest)
