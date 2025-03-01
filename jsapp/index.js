46c91d82b1eb4eace5320b188ea9ca0a8ec6324f
Will Wesley <will@knze.net>
2025-02-28 17:08:39 -0700

    An answer to the server portion of hw 4.

--- jsapp/index.js
+++ jsapp/index.js
@@ -2,7 +2,7 @@
 
 const http = require('http')
 const fs = require('node:fs');
-const { createHmac } = require('node:crypto');
+const { createHmac, randomUUID } = require('node:crypto');
 
 const secret = 'abcdefg';
 const hash = (str) =>
@@ -18,7 +18,7 @@ fs.readFile('passwd.db', 'utf8', (err, data) => {
 });
 
 
-const dancers = []
+let dancers = []
 
 const authenticate = (auth = '') => {
   const [ user, pass ] = atob(auth.slice(6)).split(':')
@@ -27,38 +27,61 @@ const authenticate = (auth = '') => {
 
 const handleRequest = (req, res) => {
   const [path, query] = req.url.split('?')
-  if(req.method == "POST") {
+  if([ 'POST', 'PUT', 'DELETE' ].includes(req.method)) {
     if(!authenticate(req.headers.authorization)) {
       res.writeHead(401, {
         "WWW-Authenticate": "Basic realm='oo laa'"
       })
       res.end()
     } else {
-      let body = ''
-      req.on('data', (data) => {
-        body += data
-      })
-      req.on('end', () => {
-        try {
-          const params = JSON.parse(body)
-          dancers.push(params)
-          kbye(res)
-        } catch {
-          res.writeHead(400)
-          res.end('Bad. Go away.')
+      let uid = query && query.match(/uid=([0-9a-f-]+)/)
+      if(req.method === 'DELETE') {
+        if(uid[1]) {
+          dancers = dancers.filter(
+            (d) => d.uid != uid[1]
+          )
+          res.writeHead(200).end()
+        } else {
+          res.writeHead(400).end()
         }
-      })
+      } else {
+        let body = ''
+        req.on('data', (data) => {
+          body += data
+        })
+        req.on('end', () => {
+          try {
+            const params = JSON.parse(body)
+            if(!uid && req.method == 'POST') {
+              uid = randomUUID()
+              dancers.push({ ...params, uid })
+              res.writeHead(201).end(uid)
+            } else if(uid && req.method == 'PUT') {
+              const i = dancers.findIndex(
+                (d) => d.uid == uid[1]
+              )
+              if(i >= 0) {
+                dancers[i] = params
+                res.writeHead(200).end()
+              } else {
+                res.writeHead(404).end()
+              }
+            } else {
+              res.writeHead(400).end()
+            }
+          } catch {
+            res.writeHead(400).end()
+          }
+        })
+      }
     }
   } else {
-    kbye(res)
+    res.writeHead(200, {
+      "Content-Type": "application/json"
+    })
+    res.write(JSON.stringify(dancers))
+    res.end()
   }
-}
-const kbye = (res) => {
-  res.writeHead(200, {
-    "Content-Type": "application/json"
-  })
-  res.write(JSON.stringify(dancers))
-  res.end()
 }
 const server = http.createServer(handleRequest)
 server.listen(3000)
